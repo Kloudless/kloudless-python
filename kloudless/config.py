b7e96a57f1b5d743ed0baad8c5395125837b2857
Vinod Chandru <vinod.chandru@gmail.com>
2014-05-31 20:56:45 -0700

    Discouraging importing configuration object directly to get existing config.

--- kloudless/config.py
+++ kloudless/config.py
@@ -1,22 +1,26 @@
-configuration = {
+_configuration = {
     'api_key': None,
     'api_version': '0',
     'base_url': 'https://api.kloudless.com'
     }
 
 def configure(**params):
-    """Update configuration based on params.
+    """Update configuration based on params. Returns new configuration.
+    To retrieve existing configuration, call this method with no arguments.
 
     :param api_key: API Key
     :param api_version: API Version
     :param base_url: Base API URL
     """
-    global configuration
-    configuration = merge(params)
-    return configuration
+    global _configuration
+    if not params:
+        return _configuration
+
+    _configuration = merge(params)
+    return _configuration
 
 def merge(config):
-    result = configuration.copy()
+    result = _configuration.copy()
     for k, v in config.iteritems():
         if k in result:
             result[k] = v
