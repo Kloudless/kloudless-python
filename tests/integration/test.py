caece78f25b4a0fffe025e411fb966636fb7404f
Timothy Liu <timothy@kloudless.com>
2016-03-29 02:19:28 -0700

    test: sorting test cases based on api and service

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -7,24 +7,44 @@ from test_cases import *
 from management_api import *
 from crm_api import *
 
+STORAGE_SERVICES = ['dropbox', 'box', 'gdrive', 'skydrive', 'sharefile',
+                    'copy', 'sugarsync', 'egnyte', 'evernote', 'sharepoint',
+                    'onedrivebiz', 'cmis', 'alfresco', 'alfresco_cloud',
+                    'salesforce', 'hubspot', 'smb', 'jive', 'webdav', 'cq5',
+                    's3', 'azure']
+CRM_SERVICES = ['salesforce', 'dynamics', 'oracle']
+
+
 def test_cases():
-    test_classes = []
+    management_tests = []
+    storage_tests = []
+    crm_tests = []
+
     for m in sys.modules.keys():
-      if '.test_' in m:
-            for name, cls in inspect.getmembers(sys.modules[m], inspect.isclass):
+        if '.test_' in m:
+            for name, cls in inspect.getmembers(sys.modules[m],
+                                                inspect.isclass):
                 if '.test_' in cls.__module__:
-                    test_classes.append(cls)
+                    if 'management_api' in cls.__module__:
+                        management_tests.append(cls)
+                    if 'crm_api' in cls.__module__:
+                        crm_tests.append(cls)
+                    if 'test_cases.' in cls.__module__:
+                        storage_tests.append(cls)
 
     cases = []
-    for cls in test_classes:
-        if utils.DEV_KEY:
-            if 'management_api.' in cls.__module__:
-                cases.append(cls)
-                continue
-        if utils.API_KEY:
-            if 'test_cases.' in cls.__module__ or 'crm_api.' in cls.__module__:
-                for acc in utils.accounts:
-                    cases.append(utils.create_test_case(acc, cls))
+    if utils.DEV_KEY:
+        cases.extend(management_tests)
+
+    if utils.API_KEY:
+        for acc in utils.accounts:
+            if acc.service in STORAGE_SERVICES:
+                for storage_test in storage_tests:
+                    cases.append(utils.create_test_case(acc, storage_test))
+
+            if acc.service in CRM_SERVICES:
+                for crm_test in crm_tests:
+                    cases.append(utils.create_test_case(acc, crm_test))
 
     return cases
 
